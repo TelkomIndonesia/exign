83e4b55224ce9fdcc4af8c891a645bd8e7063ecd
Rucciva <rucciva@gmail.com>
2022-06-30 08:57:15 +0700

    some refactor

--- dist/double-dash-domain.js
+++ dist/double-dash-domain.js
@@ -4,8 +4,9 @@ exports.mapDoubleDashDomain = exports.mapDoubleDashDomainDNS = void 0;
 const tslib_1 = require("tslib");
 const promises_1 = require("dns/promises");
 const ttlcache_1 = tslib_1.__importDefault(require("@isaacs/ttlcache"));
+const doubledash = '--';
 const doubleDashDomainDNSCache = new ttlcache_1.default({ ttl: 1000 * 60, max: 100 });
-const key = 'double-dash-domain';
+const txtKey = 'double-dash-domain';
 function mapDoubleDashDomainDNS(hostname) {
     return tslib_1.__awaiter(this, void 0, void 0, function* () {
         const v = doubleDashDomainDNSCache.get(hostname);
@@ -22,7 +23,7 @@ function mapDoubleDashDomainDNS(hostname) {
         let domain = '';
         for (const records of txt) {
             for (let value of records) {
-                value = value.startsWith(key + '=') ? value.substring(key.length + 1) : '';
+                value = value.startsWith(txtKey + '=') ? value.substring(txtKey.length + 1) : '';
                 if (!hostname.endsWith('.' + value) || value.length < domain.length) {
                     continue;
                 }
@@ -34,25 +35,30 @@ function mapDoubleDashDomainDNS(hostname) {
     });
 }
 exports.mapDoubleDashDomainDNS = mapDoubleDashDomainDNS;
-const doubledash = '--';
 function mapDoubleDashDomain(hostname, doubledashParentDomains) {
     return tslib_1.__awaiter(this, void 0, void 0, function* () {
-        if (hostname.indexOf(doubledash) < 0)
+        if (hostname.indexOf(doubledash) < 0) {
             return;
+        }
         let parentdomain = '';
         for (const v of doubledashParentDomains) {
-            if (!hostname.endsWith('.' + v) || v.length < parentdomain.length)
+            if (!hostname.endsWith('.' + v) || v.length < parentdomain.length) {
                 continue;
+            }
             parentdomain = v;
         }
         if (!parentdomain) {
             parentdomain = yield mapDoubleDashDomainDNS(hostname);
         }
-        if (!parentdomain)
+        if (!parentdomain) {
             return;
-        const part = hostname.substring(0, hostname.indexOf('.' + parentdomain)).split(/--(?=[^-.])/);
-        if (part.length === 0)
+        }
+        const part = hostname
+            .substring(0, hostname.indexOf('.' + parentdomain))
+            .split(/--(?=[^-.])/);
+        if (part.length === 0) {
             return;
+        }
         return part[part.length - 1] + '.' + parentdomain;
     });
 }
--- src/double-dash-domain.ts
+++ src/double-dash-domain.ts
@@ -1,11 +1,15 @@
 import { resolveTxt } from 'dns/promises'
 import TTLCache from '@isaacs/ttlcache'
 
+const doubledash = '--'
 const doubleDashDomainDNSCache = new TTLCache({ ttl: 1000 * 60, max: 100 })
-const key = 'double-dash-domain'
+const txtKey = 'double-dash-domain'
+
 export async function mapDoubleDashDomainDNS (hostname: string): Promise<string> {
   const v: string = doubleDashDomainDNSCache.get(hostname)
-  if (v !== undefined) { return v }
+  if (v !== undefined) {
+    return v
+  }
 
   let txt: string[][]
   try {
@@ -17,8 +21,10 @@ export async function mapDoubleDashDomainDNS (hostname: string): Promise<string>
   let domain = ''
   for (const records of txt) {
     for (let value of records) {
-      value = value.startsWith(key + '=') ? value.substring(key.length + 1) : ''
-      if (!hostname.endsWith('.' + value) || value.length < domain.length) { continue }
+      value = value.startsWith(txtKey + '=') ? value.substring(txtKey.length + 1) : ''
+      if (!hostname.endsWith('.' + value) || value.length < domain.length) {
+        continue
+      }
 
       domain = value
     }
@@ -28,23 +34,32 @@ export async function mapDoubleDashDomainDNS (hostname: string): Promise<string>
   return domain
 }
 
-const doubledash = '--'
-
 export async function mapDoubleDashDomain (hostname: string, doubledashParentDomains: string[]): Promise<string | undefined> {
-  if (hostname.indexOf(doubledash) < 0) return
+  if (hostname.indexOf(doubledash) < 0) {
+    return
+  }
 
   let parentdomain = ''
   for (const v of doubledashParentDomains) {
-    if (!hostname.endsWith('.' + v) || v.length < parentdomain.length) continue
+    if (!hostname.endsWith('.' + v) || v.length < parentdomain.length) {
+      continue
+    }
+
     parentdomain = v
   }
+  if (!parentdomain) {
+    parentdomain = await mapDoubleDashDomainDNS(hostname)
+  }
+  if (!parentdomain) {
+    return
+  }
 
-  if (!parentdomain) { parentdomain = await mapDoubleDashDomainDNS(hostname) }
-
-  if (!parentdomain) return
-
-  const part = hostname.substring(0, hostname.indexOf('.' + parentdomain)).split(/--(?=[^-.])/)
-  if (part.length === 0) return
+  const part = hostname
+    .substring(0, hostname.indexOf('.' + parentdomain))
+    .split(/--(?=[^-.])/)
+  if (part.length === 0) {
+    return
+  }
 
   return part[part.length - 1] + '.' + parentdomain
 }
